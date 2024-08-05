86088c725724e10ac1e654d5fa596a9fe4d3fa0b
Aluno <sarafabri05@gmail.com>
2024-08-05 13:41:05 -0300

    att2 do band=co de dados

--- forms_dentro_do_forms/DAO/ProfessorDAO.cs
+++ forms_dentro_do_forms/DAO/ProfessorDAO.cs
@@ -24,7 +24,14 @@ namespace forms_dentro_do_forms.DAO
         public void Inserir(ProfessoresEntidade professor)
         {
             Conexao.Open();
-            string query = "Insert into Professores";
+            string query = "Insert into Professores (Nome, Apelido) Values (@nome, @apelido)";
+            SqlCommand comando = new SqlCommand(query, Conexao);  //usa-se o @ para identificar que algo tem que ser trocado por um tipo de dados (no caso vai trocar a aparição, vai receber as variaveis) 
+            SqlParameter parametro1 = new SqlParameter("@nome", professor.Nome);
+            SqlParameter parametro2 = new SqlParameter("@apelido", professor.Apelido);
+            comando.Parameters.Add(parametro1); //garante que @apelido e @nome sejam trocados pelas informacoes necessárias
+            comando.Parameters.Add(parametro2);
+            comando.ExecuteNonQuery(); // na primeiro linha insert eu abro a conexao e o executenonquery ele vai fechar (garantindo que o projeto vai atualizar)
+            Conexao.Close();
         }
        // public void Inserir(string nome, string sigla)
        // {
